bf96a5ea09257f5f36c2bdae5627817204485122
deadman96385 <seanhoyt963@gmail.com>
2017-05-16 14:15:32 -0500

    DU-ify

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/lithium/full_lithium.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lithium
+PRODUCT_NAME := du_lithium
 PRODUCT_DEVICE := lithium
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := MI Mix
